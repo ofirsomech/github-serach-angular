f497efcfa4f13e37db2c8b248dbf1b44785f0002
Ofir Somech <ofirsomech4@gmail.com>
2023-04-20 04:16:36 +0300

    create test spec for app componnent ts

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,35 +1,23 @@
-import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
+  let component: AppComponent;
+
+  beforeEach(() => {
+    component = new AppComponent();
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+    expect(component).toBeTruthy();
   });
 
-  it(`should have as title 'GitHubSearchClient'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('GitHubSearchClient');
+  it('should return true if user is logged in', () => {
+    localStorage.setItem('token', 'test_token');
+    expect(component.isLoggedIn()).toBeTruthy();
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('GitHubSearchClient app is running!');
+  it('should return false if user is not logged in', () => {
+    localStorage.removeItem('token');
+    expect(component.isLoggedIn()).toBeFalsy();
   });
 });
